c72e6103f4bd6d09bf10d2dc696752e72b5cc232
Tayssa Passos <tayssapassoss@gmail.com>
2025-06-04 13:39:09 -0300

    update node class

--- src/atividade2/No.java
+++ src/atividade2/No.java
@@ -1,34 +1,19 @@
 package atividade2;
+
 public class No {
     int valor;
-    No esquerda, direita;
+    int altura;
+    No esquerdo, direito;
 
     public No(int valor) {
         this.valor = valor;
-        esquerda = direita = null;
+        this.altura = 1;
+        this.esquerdo = null;
+        this.direito = null;
     }
 
-    public int getValor() {
-        return valor;
-    }
-
-    public void setValor(int valor) {
-        this.valor = valor;
-    }
-
-    public No getEsquerda() {
-        return esquerda;
-    }
-
-    public void setEsquerda(No esquerda) {
-        this.esquerda = esquerda;
-    }
-
-    public No getDireita() {
-        return direita;
-    }
-
-    public void setDireita(No direita) {
-        this.direita = direita;
+    @Override
+    public String toString() {
+        return String.valueOf(valor);
     }
 }
